0713581b88446000980f9d3b7db4ee72c01c73d3
trygvmk <trygvmk@stud.ntnu.no>
2024-10-03 12:11:02 +0200

    Update providers.tf

--- Module06/src/providers.tf
+++ Module06/src/providers.tf
@@ -5,12 +5,6 @@ terraform {
        version = "4.0.1"
     }
   }
-  backend "azurerm" {
-    resource_group_name  = "rg-terraform-backend-trygvmk"
-    storage_account_name = "uhfpiuafhiug349ijf"
-    container_name       = "tfstate"
-    key                  = "web.terraform.tfstate"
-  }
 }
 
 provider "azurerm" {
